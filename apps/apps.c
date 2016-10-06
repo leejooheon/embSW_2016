d32c5956608839ee5f1a24ecadeb95a797174120
JH <sher1400@naver.com>
2016-10-06 00:54:03 -0700

    tets2

--- apps/apps.c
+++ apps/apps.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	printf("hello~\n");
 	printf("1 : debug branch~!\n");
-	printf("debug branch~!\n");
+	printf("2 : debug branch~!\n");
 //	printf("sum : %d\n", sum(10,10));
 	return 0;
 }
