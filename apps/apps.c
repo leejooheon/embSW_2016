c2326265dfb6999cafea866dafa6a7fa189c5051
JH <sher1400@naver.com>
2016-10-06 00:55:28 -0700

    merge test2

--- apps/apps.c
+++ apps/apps.c
@@ -6,6 +6,7 @@ int main(void)
 	printf("debug branch~!\n");
 	printf("debug branch~!\n");
 	printf("1 : merge test\n");
+	printf("2 : merge test\n");
 //	printf("sum : %d\n", sum(10,10));
 	return 0;
 }
