0fb36ed541bc65eee3d42cdef0e5174a00dfc909
JH <sher1400@naver.com>
2016-10-06 01:42:42 -0700

    other terminal test

--- apps/apps.c
+++ apps/apps.c
@@ -8,6 +8,7 @@ int main(void)
 	printf("1 : debug branch~!\n");
 	printf("2 : debug branch~!\n");
 	printf("github test\n");
+	printf("other terminal test\n");
 //	printf("sum : %d\n", sum(10,10));
 	return 0;
 }
